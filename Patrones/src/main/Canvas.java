d8dd52bde5680e88e0aec94d4f48d4ad0a4ee734
Diego Iván Oliveros Acosta <diegoo.ivan@gmail.com>
2017-03-14 11:58:50 -0500

    Patrones de GoF

---
+++ Patrones/src/main/Canvas.java
@@ -0,0 +1,191 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JPanel;
+
+import commands.MoveCommand;
+import common.Command;
+import common.Paintable;
+import common.PaintableFactory;
+import common.SmileConstants;
+
+public class Canvas extends JPanel {
+
+  private List<Paintable> paintableList = new ArrayList<Paintable>();
+
+  private List<Command> undoList = new ArrayList<Command>();
+  private List<Command> redoList = new ArrayList<Command>();
+
+  private Paintable draggedPaintable;
+  private Point/* */draggedBasePoint;
+
+  private int dx;
+  private int dy;
+
+  // --------------------------------------------------------------------------------
+
+  public Canvas() {
+    int x;
+    int y;
+
+    PaintableFactory paintableFactory = new PaintableFactory();
+
+    // Face 1
+    x = 100;
+    y = 100;
+    paintableList.add(paintableFactory.create(x, y, x + 100, y + 100, SmileConstants.SMILE_DW));
+
+    // Face 2
+    x = 200;
+    y = 200;
+    paintableList.add(paintableFactory.create(x, y, x + 100, y + 100, SmileConstants.SMILE_OK));
+
+    // Face 3
+    x = 300;
+    y = 300;
+    paintableList.add(paintableFactory.create(x, y, x + 100, y + 100, SmileConstants.SMILE_UP));
+
+    // --------------------------------------------------------------------------------
+    // Mouse Handling
+    // --------------------------------------------------------------------------------
+
+    addMouseMotionListener(new MouseMotionAdapter() {
+      public void mouseDragged(MouseEvent evt) {
+        clientMouseDragged(evt);
+      }
+    });
+
+    addMouseListener(new MouseAdapter() {
+      public void mousePressed(MouseEvent evt) {
+        clientMousePressed(evt);
+      }
+
+      public void mouseReleased(MouseEvent evt) {
+        clientMouseReleased(evt);
+      }
+    });
+  }
+
+  // --------------------------------------------------------------------------------
+
+  protected void clientMouseDragged(MouseEvent evt) {
+    if (draggedPaintable == null) { // GTFO
+      return;
+    }
+
+    dx = evt.getPoint().x - draggedBasePoint.x;
+    dy = evt.getPoint().y - draggedBasePoint.y;
+
+    repaint();
+  }
+
+  // --------------------------------------------------------------------------------
+
+  protected void clientMousePressed(MouseEvent evt) {
+    draggedBasePoint = evt.getPoint();
+
+    for (Paintable paintable : paintableList) {
+      if (paintable.inside(draggedBasePoint)) {
+        draggedPaintable = paintable;
+        return;
+      }
+    }
+
+    draggedBasePoint = null;
+  }
+
+  // --------------------------------------------------------------------------------
+
+  protected void clientMouseReleased(MouseEvent evt) {
+    if (draggedPaintable == null) { // GTFO
+      return;
+    }
+
+    if (dx == 0 && dy == 0) { // GTFO
+      return;
+    }
+
+    MoveCommand moveCommand = new MoveCommand(draggedPaintable, dx, dy);
+    moveCommand.redoCommand();
+
+    undoList.add(moveCommand);
+    redoList.clear();
+
+    draggedBasePoint = null;
+    draggedPaintable = null;
+
+    dx = 0;
+    dy = 0;
+
+    repaint();
+  }
+
+  // --------------------------------------------------------------------------------
+
+  @Override
+  public void paint(Graphics g) {
+    update(g);
+  }
+
+  // --------------------------------------------------------------------------------
+
+  @Override
+  public void update(Graphics g) {
+    Graphics2D g2d = (Graphics2D) g;
+
+    g2d.setBackground(Color.WHITE);
+    g2d.clearRect(0, 0, getWidth(), getHeight());
+
+    // ----------------------------------------
+
+    for (Paintable paintable : paintableList) {
+      paintable.draw(g2d);
+    }
+
+    // ----------------------------------------
+
+    if (draggedPaintable != null) {
+      g2d.setColor(Color.BLACK);
+      g2d.drawRect( //
+          draggedPaintable.getX1() + dx, //
+          draggedPaintable.getY1() + dy, //
+          draggedPaintable.getX2() - draggedPaintable.getX1(), //
+          draggedPaintable.getY2() - draggedPaintable.getY1());
+    }
+  }
+
+  // --------------------------------------------------------------------------------
+
+  public void undo() {
+    if (undoList.isEmpty()) {
+      return;
+    }
+
+    Command command = undoList.remove(undoList.size() - 1);
+    command.undoCommand();
+    redoList.add(command);
+    repaint();
+  }
+
+  // --------------------------------------------------------------------------------
+
+  public void redo() {
+    if (redoList.isEmpty()) {
+      return;
+    }
+
+    Command command = redoList.remove(redoList.size() - 1);
+    command.redoCommand();
+    undoList.add(command);
+    repaint();
+  }
+}
---
+++ Patrones/src/main/FrmMain.java
@@ -0,0 +1,71 @@
+package main;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public final class FrmMain extends JFrame {
+
+  private final Canvas client;
+
+  // --------------------------------------------------------------------------------
+
+  public FrmMain() {
+    setLayout(new BorderLayout());
+
+    client = new Canvas();
+    add(client, BorderLayout.CENTER);
+
+    add(initTopPanel(), BorderLayout.NORTH);
+
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setSize(640, 480);
+    setVisible(true);
+  }
+
+  // --------------------------------------------------------------------------------
+
+  public JPanel initTopPanel() {
+    JPanel ret = new JPanel();
+
+    JButton btnUndo = new JButton("undo");
+    btnUndo.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        btnUndoClicked();
+      }
+    });
+    ret.add(btnUndo);
+
+    JButton btnRedo = new JButton("redo");
+    btnRedo.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        btnRedoClicked();
+      }
+    });
+    ret.add(btnRedo);
+
+    return ret;
+  }
+
+  // --------------------------------------------------------------------------------
+
+  protected void btnUndoClicked() {
+    client.undo();
+  }
+
+  // --------------------------------------------------------------------------------
+
+  protected void btnRedoClicked() {
+    client.redo();
+  }
+
+  // --------------------------------------------------------------------------------
+
+  public static void main(String[] args) {
+    new FrmMain();
+  }
+}
