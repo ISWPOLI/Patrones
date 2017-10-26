d3cc764136a32a67ec39d76ebebc587e84b50634
divelasquez <30845765+divelasquez@users.noreply.github.com>
2017-10-25 21:49:46 -0500

    Cambio realizado
    Se realiza la corrección de la clase

--- Patrones2/src/tools/sad/SadPaintableFactory.java
+++ Patrones2/src/tools/sad/SadPaintableFactory.java
@@ -17,7 +17,11 @@ public class SadPaintableFactory implements PaintableFactory {
        } else {
          return new DrawnFace(x1, y1, x2, y2,SmileConstants.SMILE_UP);
 //¿Qué pasa si le asigno un numero en lugar de SmileConstants.SMILE_UP?
-//¿Porqué no genera error?
+//No genera error, Debido a que en la clase  SmileConstants ya tiene definidas por defecto  en sus variables unos números:
+/*int SMILE_UP = 0; 
+int SMILE_DW = 1;
+int SMILE_OK = 3;
+*/
         }
   }
 }
