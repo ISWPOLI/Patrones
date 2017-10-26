9c3a7b37d0420fb67a076459d68cc81673cfe84c
general <general@10.10.5.199>
2017-10-25 21:42:33 -0500

    #4 En Patrones, en el paquete common, dentro del archivo PaintableFactory.java se hace un cambio en la linea 13 ya que el if no tiene condicion por lo que se le asigna a useImage = false

--- Patrones/src/common/PaintableFactory.java
+++ Patrones/src/common/PaintableFactory.java
@@ -9,7 +9,8 @@ public class PaintableFactory {
   private boolean useImage = true;
 
   public Paintable create(int x1, int y1, int x2, int y2, int state) {
-    if (useImage) {
+      //Se le asigna la condicion al if ya que no contenia dicha condicion y generaba error
+    if (useImage = false) {
       return new ImageFace(x1, y1, x2, y2, state);
     } else {
       return new DrawnFace(x1, y1, x2, y2, state);
