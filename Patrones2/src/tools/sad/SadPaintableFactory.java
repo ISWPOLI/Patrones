f204d7e61920637632ef29d996fddefd587c438a
general <general@10.10.6.208>
2017-10-25 21:28:40 -0500

    Se modifica el valor de la cara triste, que estaba como feliz, para que de adecuadamente y se solucionan las preguntas.

--- Patrones2/src/tools/sad/SadPaintableFactory.java
+++ Patrones2/src/tools/sad/SadPaintableFactory.java
@@ -15,9 +15,13 @@ public class SadPaintableFactory implements PaintableFactory {
       if (useImage) {
     return new SadImageFace(x1, y1, x2, y2);
        } else {
-         return new DrawnFace(x1, y1, x2, y2,SmileConstants.SMILE_UP);
+         return new DrawnFace(x1, y1, x2, y2,1);
 //¿Qué pasa si le asigno un numero en lugar de SmileConstants.SMILE_UP?
+/*El metodo DrawnFace donde se creo, tiene 5 variables, y la ultima es un estado que debe ser un entero.
+El SmileConstants es una representacion de los estados de animo que se encuentra en otra clase y es un numero 1 triste, 0 feliz y 3 maso o menos. Debe ser uno de esos 3 valores.
+  */      
 //¿Porqué no genera error?
+//Porque no esta ingresando un dato diferente al que esta pidiendo el metodo el cual es un numero.
         }
   }
 }
