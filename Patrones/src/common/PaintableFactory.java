c79508952d0ac3679eb477740d86748d74ccb3e6
divelasquez <30845765+divelasquez@users.noreply.github.com>
2017-10-28 19:08:40 -0500

    Puesta a punto Patrones
    Se realiza la corrección de la condicional if

--- Patrones/src/common/PaintableFactory.java
+++ Patrones/src/common/PaintableFactory.java
@@ -9,7 +9,11 @@ public class PaintableFactory {
   private boolean useImage = true;
 
   public Paintable create(int x1, int y1, int x2, int y2, int state) {
-    if (useImage) {
+    if (useImage=false) {
+      /*Se identifica que en el paquete de common en la clase PaintableFactory  
+      en la línea 12 tiene una condicional if  pero solo está llamando la variable useaImage a la cual  
+      no le esta realizando ninguna comparacion.Por lo cual se corrige  useImage=false*/
+      
       return new ImageFace(x1, y1, x2, y2, state);
     } else {
       return new DrawnFace(x1, y1, x2, y2, state);
