7b5bc03a75df38955aaa82a182aa58e7ee99c5d2
Marynela <31083802+Marynela@users.noreply.github.com>
2017-10-31 23:53:32 -0500

    Update PaintableFactory.java
    #4 puesta patrones 1

--- Patrones/src/common/PaintableFactory.java
+++ Patrones/src/common/PaintableFactory.java
@@ -3,13 +3,12 @@ package common;
 import paintables.DrawnFace;
 import paintables.ImageFace;
 
-// Factory
 public class PaintableFactory {
 
   private boolean useImage = true;
 
   public Paintable create(int x1, int y1, int x2, int y2, int state) {
-    if (useImage) {
+    if (useImage=false) {
       return new ImageFace(x1, y1, x2, y2, state);
     } else {
       return new DrawnFace(x1, y1, x2, y2, state);
