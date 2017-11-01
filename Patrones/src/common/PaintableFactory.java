6118d21faa1b4f254a0a893e506adb57efe2dc75
dangar <>
2017-11-01 01:09:22 -0500

    #4 punto 1 completar diagrama vectorial

--- Patrones/src/common/PaintableFactory.java
+++ Patrones/src/common/PaintableFactory.java
@@ -9,10 +9,13 @@ public class PaintableFactory {
   private boolean useImage = true;
 
   public Paintable create(int x1, int y1, int x2, int y2, int state) {
-    if (useImage) {
+    if (useImage=false) {
       return new ImageFace(x1, y1, x2, y2, state);
     } else {
       return new DrawnFace(x1, y1, x2, y2, state);
     }
+    
+    /* Se compara la variable useImagen = falso en el if, y el programa compila 
+    mostrando las caras vectoriales.*/
   }
 }
